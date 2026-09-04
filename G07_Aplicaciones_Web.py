5fb4bb8facc4c50ab7882ce98ebf19b158302d90
christianveca99-beep <christian.veca99@gmail.com>
2026-09-04 16:01:27 -0500

    Update G07_Aplicaciones_Web.py

--- G07_Aplicaciones_Web.py
+++ G07_Aplicaciones_Web.py
@@ -58,25 +58,18 @@ with st.sidebar:
     st.markdown("---")
     st.markdown("**Parámetros sísmicos (Norma E.030)**")
 
-    g = st.number_input("Aceleración de la gravedad g (m/s²)", value=9.81, min_value=0.1, 
-                        step=0.01)
+    g = round(float(st.text_input("Aceleración de la gravedad g (m/s²)", value="9.81")),2)
 
     c1, c2 = st.columns(2)
 
     with c1:
-        Z = st.number_input("Factor de zonificación Z", value=0.45, min_value=0.0, 
-                            step=0.01)
-        S = st.number_input("Factor de suelo S", value=1.10, min_value=0.0, 
-                            step=0.01)
-        Tp = st.number_input("Periodo Tp (s)", value=0.6, min_value=0.01, 
-                             step=0.01)
+        Z = round(float(st.text_input("Factor de zonificación Z", value="0.45")), 2)
+        S = round(st.text_input("Factor de suelo S", value="1.10")),2)
+        Tp = round(st.text_input("Periodo Tp (s)", value="0.60")),2)
     with c2:
-        U = st.number_input("Factor de uso U", value=1.00, min_value=0.0, 
-                            step=0.01)
-        R = st.number_input("Coeficiente de reducción R", value=8.00, min_value=0.1, 
-                            step=0.1)
-        Tl = st.number_input("Periodo Tl (s)", value=2.0, min_value=0.01, 
-                             step=0.01)
+        U = round(st.text_input("Factor de uso U", value="1.00")),2)
+        R = round(st.text_input("Coeficiente de reducción R", value="8.00")),2)
+        Tl = round(st.text_input("Periodo Tl (s)", value="2.00")).2)
 
     run = st.button("Analizar", type="primary", use_container_width=True)
 
